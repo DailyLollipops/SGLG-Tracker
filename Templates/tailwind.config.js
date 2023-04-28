e4bfb340729a5c8d8b64118a8899d98bced2863a
DailyLollipops <clarencemadrigal08@gmail.com>
2023-04-28 18:10:16 +0800

    Initial Commit

---
+++ Templates/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["dist/html/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        snow: '#F1F6F9',
+        silverLakeBlue: '#5290B5',
+        policeBlue: '#394867',
+        yankeeSBlue: '#212A3E',
+        cadetBlue: '#9BA4B5'
+      },
+    },
+  },
+  plugins: [],
+}
+
