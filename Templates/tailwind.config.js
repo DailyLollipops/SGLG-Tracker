37af7eed9c10901144fd94111d328a796d97f6e5
DailyLollipops <clarencemadrigal08@gmail.com>
2023-04-30 20:39:08 +0800

    Added core functionalities for form generation and editing

--- Templates/tailwind.config.js
+++ Templates/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["dist/html/*.{html,js}"],
+  content: ["dist/**/*.{html,js}"],
   theme: {
     screens: {
       sm: '480px',
